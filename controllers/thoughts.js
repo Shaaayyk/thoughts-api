0c3807b08f69dc478eb2a1bde59f56997b61088d
Shay Kelley <shaaayyk@gmail.com>
2024-05-17 10:38:32 -0500

    update and delete for thoughts working

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -32,7 +32,20 @@ async function create(req, res) {
 
 async function update(req, res) {
   try {
-    
+    const thought = await ThoughtModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(202).json({thought})
+  } catch (error) {
+    res.status(500).json({error})
+  }
+}
+
+async function deleteThought(req, res) {
+  try {
+    const thought = await ThoughtModel.findByIdAndDelete(req.params.id)
+    if (!thought) {
+      return res.status(500).json({error})
+    }
+    res.status(202).json({thought})
   } catch (error) {
     res.status(500).json({error})
   }
@@ -42,4 +55,6 @@ module.exports = {
   index,
   show,
   create,
+  update,
+  delete: deleteThought,
 }
--- routes/thoughts.js
+++ routes/thoughts.js
@@ -6,5 +6,7 @@ const thoughtController = require('../controllers/thoughts.js')
 router.get('/', thoughtController.index)
 router.get('/:id', thoughtController.show)
 router.post('/', thoughtController.create)
+router.put('/:id', thoughtController.update)
+router.delete('/:id', thoughtController.delete)
 
 module.exports = router
