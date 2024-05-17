59de773e0307fe5207effd06add9e73083ed9d36
Shay Kelley <shaaayyk@gmail.com>
2024-05-16 20:27:33 -0500

    tested thoughts model and controller with postman

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express')
-const path = require('path')
 const logger = require('morgan')
 
 require('dotenv').config();
-
 require('./config/database');
 
+const thoughtsRouter = require('./routes/thoughts.js')
 
 const app = express()
 
 app.use(logger('dev'))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/*', (req, res) => {
-  res.send('Hello there!');
-});
+app.use('/api/thoughts', thoughtsRouter);
+
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Express app running on port ${port}`)
 });
+
+module.exports = app
