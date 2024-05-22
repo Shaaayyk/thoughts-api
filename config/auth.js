6236517d9187d9174a061a5a0b1f2360dc1a65b3
Shay Kelley <shaaayyk@gmail.com>
2024-05-21 20:56:19 -0500

    added cors and updated jwt verify middleware

--- config/auth.js
+++ config/auth.js
@@ -2,17 +2,20 @@ const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
   let token = req.get('Authorization') || req.query.token || req.body.token
-
   if (token) {
     token = token.replace('Bearer ', '')
-    jwt.verify(token, process.env.SECRET_KEY, function (error, decoded) {
-      if (error) {
-        next(error)
-      } else {
-        req.user = decoded.user
-        next()
-      }
-    })
+    if (token === 'null') {
+      next()
+    } else {
+      jwt.verify(token, process.env.SECRET_KEY, function (error, decoded) {
+        if (error) {
+          next(error)
+        } else {
+          req.user = decoded.user
+          next()
+        }
+      })
+    }
   } else {
     next()
   }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
 
 // env variables
 require('dotenv').config();
@@ -13,6 +14,7 @@ const additionsRouter = require('./routes/additions.js')
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
